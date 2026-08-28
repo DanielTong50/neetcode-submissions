30b57601d94e4b585ee5f674d2ac87ff531fca7f
Daniel Tong <97985602+DanielTong50@users.noreply.github.com>
2026-08-27 19:40:22 -0700

    Add: replace-elements-with-greatest-element-on-right-side - submission-3

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        result = []
+        largest = -1
+        for num in reversed(arr):
+            result.append(largest)
+            if num >= largest:
+                largest = num
+        
+        result.reverse()
+
+        return result
