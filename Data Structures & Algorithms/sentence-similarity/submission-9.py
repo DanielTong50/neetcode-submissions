e1828254bb77368b2e6644fb47c55999e99a4548
Daniel Tong <97985602+DanielTong50@users.noreply.github.com>
2026-08-29 20:15:06 -0700

    Add: sentence-similarity - submission-9

---
+++ Data Structures & Algorithms/sentence-similarity/submission-9.py
@@ -0,0 +1,15 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        #go through list of lists -> similar pairs
+        if len(sentence1) != len(sentence2):
+            return False
+        #we need a set of all the values to check membership
+        dictionary = set(map(tuple, similarPairs))
+
+        for i in range(len(sentence1)):
+            if sentence2[i] != sentence1[i]:
+                if (sentence1[i], sentence2[i]) not in dictionary and (sentence2[i], sentence1[i]) not in dictionary:
+                    return False
+        return True
+            
+        
